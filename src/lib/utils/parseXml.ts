913baa8470eded820c454c286cb11eb2be302dfa
Rodrigo Limões <rolimoes@gmail.com>
2023-08-10 15:11:24 -0300

    refactor: Adding AoeffModel; Adding ArchiModel

--- src/lib/utils/parseXml.ts
+++ src/lib/utils/parseXml.ts
@@ -1,38 +1,13 @@
 import xml2js from 'xml2js';
-
-interface ArchimateXmlModel {
-  'xmlns:xsi': string;
-  'xmlns:archimate': string;
-  name: string;
-  id: string;
-  version: string;
-}
-
-interface AoffXmlModel {
-  identifier: string;
-  xmlns: string;
-  'xmlns:xsi': string;
-  'xsi:schemaLocation': string;
-}
-
-export interface ArchimateModel {
-  'archimate:model': {
-    $: ArchimateXmlModel;
-  };
-}
-
-export interface AoffModel {
-  model: {
-    $: AoffXmlModel;
-  };
-}
+import { AoeffModel } from '@lib/interfaces/AoeffModel';
+import { ArchiModel } from '@lib/interfaces/ArchiModel';
 
 /**
  * Returns a json xml template from the xml file buffer
  * @param fileString File buffer
  */
 export async function parseXml(fileString: Buffer) {
-  return new Promise<AoffModel | ArchimateModel>((resolve, reject): void => {
+  return new Promise<AoeffModel | ArchiModel>((resolve, reject): void => {
     let parser = new xml2js.Parser({ explicitArray: true });
 
     parser.parseString(fileString, function (error, result) {
