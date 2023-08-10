754a7dda56fc7fb87e7a7e10dbf11550bf10a206
Rodrigo Limões <rolimoes@gmail.com>
2023-08-10 15:30:55 -0300

    refactor: Adding checkFileType function; Adding isAoeffXmlClassifier function; Adding isArchiFileVersionSupported function

--- src/lib/processors/InputProcessingDirector/InputProcessorDirector.ts
+++ src/lib/processors/InputProcessingDirector/InputProcessorDirector.ts
@@ -5,17 +5,19 @@ import InputTranslator from '@lib/processors/InputTranslator/InputTranslator';
 import Archi4Interpreter from '@lib/processors/InputTranslator/interpreter/fileBasedInterpreter/archi/Archi4Interpreter';
 import AoeffInterpreter from '@lib/processors/InputTranslator/interpreter/fileBasedInterpreter/aoeff/AoeffInterpreter';
 import { parseXml } from '@lib/utils/parseXml';
+import { AoeffModel } from '@lib/interfaces/AoeffModel';
+import { ArchiModel } from '@lib/interfaces/ArchiModel';
 
 interface Option {
   skipViews: boolean;
 }
 
-const AOEFF_XML_CLASSIFIER = 'http://www.opengroup.org/xsd/archimate/3.0/';
+type Log = (message?: string, ...optionalParams: Array<any>) => void;
 
 export class InputProcessorDirector {
-  private readonly model: any;
+  private readonly model: any; //TODO: Add Model Type
   private readonly options: Option;
-  private readonly log: any;
+  private readonly log: Log;
 
   constructor(
     {
@@ -25,7 +27,7 @@ export class InputProcessorDirector {
         skipViews: false,
       },
     },
-    logger?: any,
+    logger?: Console,
   ) {
     if (options.skipViews === undefined) {
       throw new Error(`Invalid options`);
@@ -36,45 +38,66 @@ export class InputProcessorDirector {
     this.log = logger && logger.info ? logger.info : () => {};
   }
 
-  async translateModelFile(filePath) {
+  private static checkFileType(xmlFile: AoeffModel | ArchiModel) {
+    return (modelKey: string): boolean => modelKey in xmlFile && xmlFile[modelKey] !== undefined;
+  }
+
+  private static isAoeffXmlClassifier(modelType: string): boolean {
+    const aoeffXmlClassifier = 'http://www.opengroup.org/xsd/archimate/3.0/';
+    return modelType.localeCompare(aoeffXmlClassifier) === 0;
+  }
+
+  private static isArchiFileVersionSupported(archimateModel: ArchiModel): boolean {
+    const archimateXmlClassifier = 'http://www.archimatetool.com/archimate';
+    const model = archimateModel['archimate:model'];
+    const modelVersion = model.$.version;
+
+    return (
+      model.$['xmlns:archimate'].localeCompare(archimateXmlClassifier) === 0 &&
+      (modelVersion.startsWith('4.0') || // Tested
+        modelVersion.startsWith('4.1') ||
+        modelVersion.startsWith('4.2') ||
+        modelVersion.startsWith('4.3') ||
+        modelVersion.startsWith('4.4') || // Tested
+        modelVersion.startsWith('4.5') ||
+        modelVersion.startsWith('4.6') || // Tested
+        modelVersion.startsWith('4.7') ||
+        modelVersion.startsWith('4.8') ||
+        modelVersion.startsWith('4.9')) // Tested
+    );
+  }
+
+  async translateModelFile(filePath): Promise<void> {
     let fileString = fs.readFileSync(filePath);
 
     let xmlFile = await parseXml(fileString);
 
     if (xmlFile !== null) {
       let interpreter = null;
+      const checkFileType = InputProcessorDirector.checkFileType(xmlFile);
+      const isAoeffFile = checkFileType('model');
 
       // Verifying if it's a AOEFF file
-      if ('model' in xmlFile && xmlFile.model !== undefined) {
-        let modelType = xmlFile.model.$.xmlns;
+      if (isAoeffFile) {
+        const aoeffModel = xmlFile as AoeffModel;
+        const modelType = aoeffModel.model.$.xmlns;
 
-        if (modelType.localeCompare(AOEFF_XML_CLASSIFIER) === 0) {
-          interpreter = new AoeffInterpreter(xmlFile);
+        if (InputProcessorDirector.isAoeffXmlClassifier(modelType)) {
+          interpreter = new AoeffInterpreter(aoeffModel);
         }
       } else {
-        // Verifying if it's an Archi 4 (Archimate 3) file
-        if ('archimate:model' in xmlFile && xmlFile['archimate:model'] !== undefined) {
-          let modelVersion = xmlFile['archimate:model'].$.version;
+        const modelKey = 'archimate:model';
+        const isArchiFile = checkFileType(modelKey);
 
-          if (
-            xmlFile['archimate:model'].$['xmlns:archimate'].localeCompare(
-              'http://www.archimatetool.com/archimate',
-            ) === 0 &&
-            (modelVersion.startsWith('4.0') || // Tested
-              modelVersion.startsWith('4.1') ||
-              modelVersion.startsWith('4.2') ||
-              modelVersion.startsWith('4.3') ||
-              modelVersion.startsWith('4.4') || // Tested
-              modelVersion.startsWith('4.5') ||
-              modelVersion.startsWith('4.6') || // Tested
-              modelVersion.startsWith('4.7') ||
-              modelVersion.startsWith('4.8') ||
-              modelVersion.startsWith('4.9')) // Tested
-          ) {
-            interpreter = new Archi4Interpreter(xmlFile);
+        // Verifying if it's an Archi 4 (Archimate 3) file
+        if (isArchiFile) {
+          const archimateModel = xmlFile as ArchiModel;
+
+          if (InputProcessorDirector.isArchiFileVersionSupported(archimateModel)) {
+            interpreter = new Archi4Interpreter(archimateModel);
           } else {
             throw new Error(
-              `Archi file version not supported - Version ${xmlFile['archimate:model'].$.version}`,
+              `Archi file version not supported - Version ${archimateModel[modelKey].$.version}`,
             );
           }
         }
@@ -99,7 +122,7 @@ export class InputProcessorDirector {
     }
   }
 
-  async translateModelFolder(folderPath) {
+  async translateModelFolder(folderPath): Promise<void> {
     // TODO: verify if really is a GRAFICO model
     try {
       let interpreter = new Grafico(folderPath);
