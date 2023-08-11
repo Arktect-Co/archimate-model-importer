6b4cb561a9b865d4e1574fee598de4ab1f5197bb
Rodrigo Limões <rolimoes@gmail.com>
2023-08-11 14:31:34 -0300

    refactor: Adding index file with all interfaces of Model

---
+++ src/lib/interfaces/model/index.ts
@@ -0,0 +1,22 @@
+import { Model, Nodes, TotalByType, Relationships } from '@lib/interfaces/model/Model';
+import { View, ViewNode, ViewRelationship, BendPoint, Bounds } from '@lib/interfaces/model/View';
+import { Statistics } from '@lib/interfaces/model/Statistics';
+import { Node } from '@lib/interfaces/model/Node';
+import { Relationship } from '@lib/interfaces/model/Relationship';
+import { Landscape } from '@lib/interfaces/model/Landscape';
+
+export {
+  Model,
+  Nodes,
+  TotalByType,
+  Relationships,
+  View,
+  ViewNode,
+  ViewRelationship,
+  BendPoint,
+  Bounds,
+  Statistics,
+  Landscape,
+  Relationship,
+  Node,
+};
