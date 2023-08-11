fc859129846864c66f4b07d40190dae07e400267
Rodrigo Limões <rolimoes@gmail.com>
2023-08-11 16:30:30 -0300

    test: Ensure return a view element with parent

--- __tests__/unit/lib/models/Model.test.ts
+++ __tests__/unit/lib/models/Model.test.ts
@@ -6,7 +6,7 @@ use(deepEqualInAnyOrder);
 
 describe('Model', () => {
   describe('createViewElement', () => {
-    const viewElementInput = {
+    const elementInput = {
       type: 'group',
       modelNodeId: '93d885b0',
       height: 100,
@@ -16,22 +16,33 @@ describe('Model', () => {
       viewNodeId: 'ebc607cd',
     };
     it('should return a view element with the default name if the name is not defined', () => {
-      const viewElement = Model.createViewElement(viewElementInput);
+      const viewElement = Model.createViewElement(elementInput);
 
       expect(viewElement).to.deep.equalInAnyOrder({
         name: 'Unknown Name',
-        ...viewElementInput,
+        ...elementInput,
         parent: undefined,
+      });
+    });
+
+    it('should return a view element with parent', () => {
+      const parent = '94d885b0';
+      const viewElement = Model.createViewElement({ ...elementInput, parent });
+
+      expect(viewElement).to.deep.equalInAnyOrder({
+        name: 'Unknown Name',
+        ...elementInput,
+        parent,
       });
     });
 
     it('should return a view element', () => {
       const name = 'View Element 1';
-      const viewElement = Model.createViewElement({ ...viewElementInput, name });
+      const viewElement = Model.createViewElement({ ...elementInput, name });
 
       expect(viewElement).to.deep.equalInAnyOrder({
         name,
-        ...viewElementInput,
+        ...elementInput,
         parent: undefined,
       });
     });
