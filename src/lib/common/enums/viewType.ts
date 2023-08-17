f582432820fc8527cb728d9517502fd3e8ae58f5
Rodrigo Limões <rolimoes@gmail.com>
2023-08-16 21:26:43 -0300

    refactor:  Adding Aoeff view type enum

--- src/lib/common/enums/viewType.ts
+++ src/lib/common/enums/viewType.ts
@@ -1,5 +1,10 @@
-export enum ViewType {
+export enum AoeffViewType {
   Element = 'Element',
   Label = 'Label',
   Container = 'Container',
+}
+
+export enum ViewType {
+  Group = 'group',
+  Note = 'note',
 }
