7e4976427ec0321f5837155c56726e732a5230e1
Rodrigo Limões <rolimoes@gmail.com>
2023-08-19 17:04:32 -0300

    refactor: Adding Node interface

---
+++ src/lib/common/interfaces/graficoModel/Node.ts
@@ -0,0 +1,21 @@
+interface NodeIdentification {
+  'xmlns:archimate': string;
+  name: string;
+  id: string;
+  type: string;
+  documentation?: string;
+}
+
+interface PropertySetting {
+  key: string;
+  value: string;
+}
+
+export interface Property {
+  $: PropertySetting;
+}
+
+export interface Node {
+  $: NodeIdentification;
+  properties?: Array<Property>;
+}
