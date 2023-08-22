460a14d4146e1a05df15086a16fb01a634268bca
Rodrigo Limões <rolimoes@gmail.com>
2023-08-22 16:38:37 -0300

    test: Ensure importing AOEFF for Archimate 3.1 - From Archi v5

--- __tests__/unit/model_translation.test.ts
+++ __tests__/unit/model_translation.test.ts
@@ -8,8 +8,40 @@ import { modelResultArchi5 } from './modelResults/modelResultArchi5';
 
 import { InputProcessorDirector } from '@lib/processors/InputProcessingDirector/InputProcessorDirector';
 import { Landscape, View } from '../../src/lib/common/interfaces/model';
+import { Model } from '../../src/lib/models/Model';
 
 chai.use(deepEqualInAnyOrder);
+
+/**
+ * Removes the relationship and return a model copy.
+ * WARNING! Handling AOEFF limitations: Not presenting hided view relationships. This kind of situation isn't
+ * exported in AOEFF, so in oder to test the import feature, the removal of this kind of relationships it is necessary
+ * @param model model result
+ */
+function removeViewRelationship(model: Partial<Model>): Model {
+  let deepCopyModelResult = JSON.parse(JSON.stringify(model));
+
+  // Finding view with hided relationships
+  let index = deepCopyModelResult.model.views.findIndex(view => view.name === 'Relationships');
+
+  if (index !== -1) {
+    // Finding and Removing Grouping -> Goal relationship
+    deepCopyModelResult.model.views[index].viewRelationships = deepCopyModelResult.model.views[
+      index
+    ].viewRelationships.filter(rel => {
+      return rel.viewRelationshipId !== '6ac44a09-fb7a-4d2f-94da-0a45854b63cb';
+    });
+
+    // Finding and Removing Grouping -> Driver relationship
+    deepCopyModelResult.model.views[index].viewRelationships = deepCopyModelResult.model.views[
+      index
+    ].viewRelationships.filter(rel => {
+      return rel.viewRelationshipId !== 'd45c553a-9a77-4ced-a77b-7b9ebfaa0743';
+    });
+  }
+
+  return deepCopyModelResult;
+}
 
 /**
  * Removes folders Id
@@ -163,26 +195,34 @@ describe('Model Translation', () => {
 
       // WARNING! Handling AOEFF limitations: Not presenting hided view relationships. This kind of situation isn't
       // exported in AOEFF, so in oder to test the import feature, the removal of this kind of relationships it is necessary
-      let deepCopyModelResult = JSON.parse(JSON.stringify(modelResultArchi));
+      const deepCopyModelResult = removeViewRelationship(modelResultArchi);
 
-      // Finding view with hided relationships
-      let index = deepCopyModelResult.model.views.findIndex(view => view.name === 'Relationships');
+      chai.expect(response).to.deep.equalInAnyOrder(deepCopyModelResult);
+    });
 
-      if (index !== -1) {
-        // Finding and Removing Grouping -> Goal relationship
-        deepCopyModelResult.model.views[index].viewRelationships = deepCopyModelResult.model.views[
-          index
-        ].viewRelationships.filter(rel => {
-          return rel.viewRelationshipId !== '6ac44a09-fb7a-4d2f-94da-0a45854b63cb';
-        });
+    it('Importing AOEFF for Archimate 3.1 - From Archi v5', async () => {
+      let inputProcessorDirector = new InputProcessorDirector({
+        label: 'Archi Test v5',
+        description: 'Test model for Archi Files',
+      });
 
-        // Finding and Removing Grouping -> Driver relationship
-        deepCopyModelResult.model.views[index].viewRelationships = deepCopyModelResult.model.views[
-          index
-        ].viewRelationships.filter(rel => {
-          return rel.viewRelationshipId !== 'd45c553a-9a77-4ced-a77b-7b9ebfaa0743';
-        });
-      }
+      await inputProcessorDirector.translateModelFile(
+        path.join(path.dirname(__filename), '/models/aoeff_3_1_archi_5_0_2.xml'),
+      );
+
+      let response = inputProcessorDirector.getOutputModel();
+
+      delete response.modelsourceid;
+
+      // Removing Folder ids
+      removeFolderIds(response.model.landscape);
+
+      // Removing [undefined] values in View Relationships for isBidirectional
+      removeUndefinedValues(response.model.views);
+
+      // WARNING! Handling AOEFF limitations: Not presenting hided view relationships. This kind of situation isn't
+      // exported in AOEFF, so in oder to test the import feature, the removal of this kind of relationships it is necessary
+      const deepCopyModelResult = removeViewRelationship(modelResultArchi5);
 
       chai.expect(response).to.deep.equalInAnyOrder(deepCopyModelResult);
     });
@@ -209,26 +249,7 @@ describe('Model Translation', () => {
 
       // WARNING! Handling AOEFF limitations: Not presenting hided view relationships. This kind of situation isn't
       // exported in AOEFF, so in oder to test the import feature, the removal of this kind of relationships it is necessary
-      let deepCopyModelResult = JSON.parse(JSON.stringify(modelResultVisualParadigm));
-
-      // Finding view with hided relationships
-      let index = deepCopyModelResult.model.views.findIndex(view => view.name === 'Relationships');
-
-      if (index !== -1) {
-        // Finding and Removing Grouping -> Goal relationship
-        deepCopyModelResult.model.views[index].viewRelationships = deepCopyModelResult.model.views[
-          index
-        ].viewRelationships.filter(rel => {
-          return rel.viewRelationshipId !== '6ac44a09-fb7a-4d2f-94da-0a45854b63cb';
-        });
-
-        // Finding and Removing Grouping -> Driver relationship
-        deepCopyModelResult.model.views[index].viewRelationships = deepCopyModelResult.model.views[
-          index
-        ].viewRelationships.filter(rel => {
-          return rel.viewRelationshipId !== 'd45c553a-9a77-4ced-a77b-7b9ebfaa0743';
-        });
-      }
+      const deepCopyModelResult = removeViewRelationship(modelResultVisualParadigm);
 
       chai.expect(response).to.deep.equalInAnyOrder(deepCopyModelResult);
     });
